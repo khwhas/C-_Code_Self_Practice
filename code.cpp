0bf216103517b467e4ea8b2815ac41b4b5af25d1
khwhas <khwaja.idrees96@gmail.com>
2024-02-16 05:04:53 +0500

    First commit

---
+++ code.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+    double x, y, sum, x_1;
+    
+    x = 6;
+    y = 50;
+    x_1 = 3;
+
+  //  cout << "Enter the value of x: ";
+  //  cin >> x;
+    
+    
+  //  cout << "The value of x is incremented by on value =";
+  //  x_1 = ++x;
+  //  cout << x_1 << endl;
+     
+//    cout << "Enter the value of y: ";
+//    cin >> y;
+
+//    sum = x + y;
+
+//    cout << "The sum of x and y is = " << sum << endl;
+
+//      if (x > y) {
+//	cout << "x is greater than y" << endl;
+//      }
+//      else if (x < y) {
+//	cout << "x is smaller than y" << endl;
+//	}
+//      else {
+//      cout << "The values of x and y are same" << endl; 
+//      }
+
+
+// String variable
+
+//	string this_is_string = "My name is Hassan";
+//	cout << this_is_string << endl;
+
+//Short Hand if .... else statment
+
+//	x = 10;
+//	string output = (x > 15) ? "The number is greater than 15" : "The number is lower than 15";
+//	cout << output << endl;
+
+// Switching the statments
+
+
+//	switch (x) {
+//		case 1 :
+//		cout << "Friday";
+//		break;
+//		case 2:
+//		cout << "Sunday";
+//		break;
+//	}
+
+
+// While Loop 
+	int i = 1;
+	while (i < 7) {
+	cout << i << "\n";
+	i++; 
+	}
+
+
+     return 0;
+}
+
